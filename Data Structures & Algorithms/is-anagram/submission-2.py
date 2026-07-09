2c97a084b9c4b4e8030014989c4dc5e41c4bff91
chanvinay <51709590+chanvinay@users.noreply.github.com>
2026-07-09 10:05:50 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        freq=defaultdict(int)
+        for char in s:
+            freq[char]+=1
+        for char in t:
+            freq[char]-=1
+
+        for count in freq.values():
+            if count!=0:
+                return False
+
+        return True
+        
